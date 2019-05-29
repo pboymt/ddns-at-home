b0cdebfc1e7e037055112095c89203a8c9c05c33
Robert Lee <pboymt@foxmail.com>
2019-05-29 12:01:01 +0800

    尝试不做任何chmod更改

--- set-crontab.sh
+++ set-crontab.sh
@@ -8,6 +8,6 @@ echo -e $cronsh >> $shpath
 
 chmod +x $shpath
 
-cronjob="*/15 * * * * $shpath >> $PWD/ddns.log"
+cronjob="*/15 * * * * sh $shpath >> $PWD/ddns.log"
 
 (crontab -u $USER -l; echo "$cronjob" ) | crontab -u $USER -
